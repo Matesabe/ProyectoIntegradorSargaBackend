8201e9dd68b3db82d0bcfb36276ab1ef98a8bb03
Mateo De María <mateodemaria51@gmail.com>
2025-06-02 13:56:37 -0300

    seed

--- Infrastructure/DataAccess/EF/SeedData.cs
+++ Infrastructure/DataAccess/EF/SeedData.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using BusinessLogic.Entities;
+using BusinessLogic.VO;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,26 @@ using System.Threading.Tasks;
 
 namespace Infrastructure.DataAccess.EF
 {
-    internal class SeedData
+    public class SeedData
     {
+        private SargaContext _context;
+
+        public SeedData(SargaContext context)
+        {
+            _context = context;
+        }
+
+        public void Run()
+        {
+            if (!_context.Administrators.Any()) loadAdmin();
+        }
+
+        private void loadAdmin()
+        {
+            Administrator unA = null;
+            unA = new Administrator(0, "87654321", new Name("Administrador Sarga"), new Password("SargaPass983"), new Email("adminSarga@mail.com"), "123456789", "Administrator");
+            _context.Administrators.Add(unA);
+            _context.SaveChanges();
+        }
     }
 }
